e96710032390bb1273b145821288d8bf9a97ac5a
Jhin <engkim0826@hotmail.com>
2021-04-10 22:37:18 +0900

    change the resolver to fetch data from database

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,16 +3,10 @@ const { User } = require('../models');
 const resolvers = {
   Query: {
     getUsers: () => {
-      const users = [
-        {
-          id:1,
-          username: 'jin',
-          email: 'test@hotmail.co.jp' 
-        }
-      ];
-      return users;
+      User.findAll().then(users => users)
+        .catch(error => error.message);
     }
-  },
+  }
 };
 
 module.exports = resolvers;
