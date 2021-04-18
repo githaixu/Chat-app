16d07eafc7d77ec1050d8f516e39bbdc237d8ad4
Jhin <engkim0826@hotmail.com>
2021-04-18 14:33:18 +0900

    add validation and errors object 2642

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
 const bcrypt = require('bcryptjs');
+const { UserInputError } = require('apollo-server');
 
 const resolvers = {
   Query: {
@@ -22,23 +23,23 @@ const resolvers = {
         if(username.trim() === '') errors.username = 'username must not be empty';
         if(password.trim() === '') errors.password = 'password must not be empty';
         if(confirmPassword.trim() === '') errors.confirmPassword = 'confirmPassword must not be empty';
+        if(password !== confirmPassword) errors.confirmPassword = 'confirmPassword must match password';
         // check if username and email already exists
         const hasUsername = await User.findOne({where: { username }});
-        const hasEmail = await User.findOne({where: { Email }});
+        const hasEmail = await User.findOne({where: { email }});
+         
         if(hasUsername) errors.username = 'username is taken';
         if(hasEmail) errors.email = 'email is taken';
 
         if(Object.keys(errors).length > 0) throw errors;
         // hash password
         password = await bcrypt.hash(password, 6);
-        // check if the username / email exists
         // create user
         const user = await User.create({username, email, password});
         // return user auto called to toJASON
         return user;
-      } catch (err) {
-        console.log(err);
-        throw err
+      } catch (errors) {
+        throw new UserInputError('bad input', { errors });
       }
     }
   }
