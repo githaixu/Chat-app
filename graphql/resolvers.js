6a57e595bac46257deee6f6313b2e7586ce4065a
Jhin <engkim0826@hotmail.com>
2021-04-25 19:21:08 +0900

    add some comments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -13,16 +13,20 @@ const resolvers = {
     }
   },
   Mutation: {
+    // since we are communicate with the DB, we are useing the async await function
     register: async (parent, args, { db }, info) => {
       let { username, email, password, confirmPassword } = args;
       let errors = {};
       try {
         // validate input data
+        // step 1 check if the input is empty 
         if(email.trim() === '') errors.email = 'email must not be empty';
         if(username.trim() === '') errors.username = 'username must not be empty';
         if(password.trim() === '') errors.password = 'password must not be empty';
         if(confirmPassword.trim() === '') errors.confirmPassword = 'confirmPassword must not be empty';
+        // also check if the comfirmed password is equl to password
         if(password !== confirmPassword) errors.confirmPassword = 'confirmPassword must match password';
+
         // check if username and email already exists
         const hasUsername = await db.User.findOne({where: { username }});
         const hasEmail = await db.User.findOne({where: { email }});
@@ -30,13 +34,17 @@ const resolvers = {
         if(hasUsername) errors.username = 'username is taken';
         if(hasEmail) errors.email = 'email is taken';
 
-        if(Object.keys(errors).length > 0) throw errors;
-        // hash password
-        password = await bcrypt.hash(password, 6);
-        // create user
-        const user = await db.User.create({username, email, password});
-        // return user auto called to toJASON
-        return user;
+        // if there is no errors
+        if(Object.keys(errors).length > 0) {
+          throw errors;
+        } else {
+          // hash password
+          password = await bcrypt.hash(password, 6);
+          // create user
+          const user = await db.User.create({username, email, password});
+          // return user auto called to toJASON
+          return user;
+        }
       } catch (errors) {
         throw new UserInputError('bad input', { errors });
       }
